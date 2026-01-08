1ee30af8029e713f7c7bb74542516d227bbd4e65
poojavgupta <poojaagupta48@gmail.com>
2026-01-08 14:46:39 -0800

    started with exploring vite

---
+++ src/Navigation/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer(){
+    return(
+        <>
+        <h4>All Rights Reserved @2026</h4>
+        </>
+    )
+}
---
+++ src/Navigation/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return(
+        <>
+        <h1>Hello Pooja, Welcome to your first react + vite application.....</h1>
+        </>
+    )
+}
